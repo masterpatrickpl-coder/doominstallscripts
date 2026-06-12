7854288d9a174f0fc52f3d44a1491af92b1538b5
KAMIkaZe <masterpatrick.pl@gmail.com>
2026-06-12 16:04:59 +0800

    update doomsetup.ps1

--- doomsetup.ps1
+++ doomsetup.ps1
@@ -3,19 +3,25 @@ Unregister-ScheduledTask -TaskName "doomsetup" -Confirm:$false
 
 $pathofscript = "c:\doomsetupcontinue.sh"
 
-$shContent = @"
+$shContent = @'
 #!/bin/bash
 apt update && apt upgrade -y
 apt install -y build-essential git libx11-dev libxext-dev wget
 git clone https://github.com/id-Software/DOOM.git ~/DOOM
 cd ~/DOOM/linuxdoom-1.10
 mkdir -p linux
-make
 rm -f linux/*.o
+dpkg --add-architecture i386
+apt update
 apt install -y gcc-multilib libc6-dev-i386 libx11-dev:i386 libxext-dev:i386 xserver-xephyr
 sed -i '1s/^/#include <errno.h>\n/' i_sound.c
 sed -i 's/CFLAGS=-g -Wall -DNORMALUNIX -DLINUX # -DUSEASM/CFLAGS=-g -Wall -DNORMALUNIX -DLINUX -m32 -Wno-implicit-function-declaration -Wno-implicit-int # -DUSEASM/' Makefile
-sed -i 's/$(CC) $(CFLAGS) $(LDFLAGS) $(OBJS) $(O)/i_main.o \/$(CC) -m32 $(CFLAGS) $(LDFLAGS) $(OBJS) $(O)/i_main.o \' Makefile
-"@
+sed -i 's|$(CC) $(CFLAGS) $(LDFLAGS)|$(CC) -m32 $(CFLAGS) $(LDFLAGS)|' Makefile
+make
+apt install doom-wad-shareware -y
+
+'@
 $bytes = [System.Text.Encoding]::UTF8.GetBytes($shContent.Replace("`r`n", "`n"))
 [System.IO.File]::WriteAllBytes($pathofscript, $bytes)
+
+
