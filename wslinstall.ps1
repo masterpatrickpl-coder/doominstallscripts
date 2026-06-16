02e69fc078e92c0c8182b45055c84e83e0c31184
PaymentBypass <236301863+masterpatrickpl-coder@users.noreply.github.com>
2026-06-16 15:15:36 +0800

    Change log message color from Orange to Yellow

--- wslinstall.ps1
+++ wslinstall.ps1
@@ -15,7 +15,7 @@ $pwsh7path32 = "C:\Program Files (x86)\PowerShell\7"
 if ((Test-path $pwsh7path) -or (Test-Path $pwsh7path32)) {
 $getpwshversion = pwsh -command '$PSVersionTable.PSVersion.ToString()'
 if ($getpwshversion -ne "7.6.2") {
-write-host "powershell7 install found but not latest, upgrading" -ForegroundColor Orange
+write-host "powershell7 install found but not latest, upgrading" -ForegroundColor Yellow
 winget upgrade --id Microsoft.Powershell --silent --verbose --accept-source-agreements --accept-package-agreements --include-unknown
 } 
 else {
@@ -23,7 +23,7 @@ write-host "powershell okay" -ForegroundColor Green
 }
 }
 else {
-write-host "no powershell7 install found, installing..." -ForegroundColor Orange
+write-host "no powershell7 install found, installing..." -ForegroundColor Yellow
 winget install --id Microsoft.Powershell --silent --verbose --accept-source-agreements --accept-package-agreements --include-unknown
 }
 
